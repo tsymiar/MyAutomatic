544304a4e4a0202ccf80776d217060d000378294
tsymiar <909006258@qq.com>
2016-06-13 00:37:15 +0800

    一种C/S模式AES加密验证机制

---
+++ AESwithMD5/AESwithMD5.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <aes.h>
+#include <md5.h>
+#include "transcoding.h"
+int main(int argc, char *argv[]) {
+	int i,len;
+	char in[256];
+	char out[64];
+	char key[16];
+	uint8_t *ek = 0;
+	uint8_t cipher[32]; // 128
+	uint8_t encrypt[64];
+	while (1)
+	{
+		printf("请输入要计算MD5值的字符串:\n");
+		gets_s(in);
+		md5_str(in, key);
+		//printf("MD5:");
+		//for (i = 0; i<16; i++) //16位无符号整数
+		//	printf("%02x", (unsigned char)key[i]);
+		//printf("\n");
+		hex_to_str((unsigned char*)key, out);
+		printf("MD5:%s\n", out);
+		printf("以MD5值为密钥\n");
+		len=key_len((uint8_t *)key);
+		ek = (uint8_t*)malloc(len);
+		key_exp((uint8_t*)key, ek);
+		hex_to_str((unsigned char*)key, out);
+		printf("输入:%s\n", out);
+		en_cipher((uint8_t*)key, cipher, ek);
+		hex_to_str(cipher, out);
+		printf("密文:%s\n", out);
+		inv_cipher(cipher, encrypt, ek);
+		hex_to_str(encrypt, out);
+		printf("解密:%s\n", out);
+		//getchar();
+		printf("\n");
+	}
+	//exit(0);
+}
---
+++ AESwithMD5/transcoding.h
@@ -0,0 +1,22 @@
+#ifndef TRANSCODING_H
+#define TRANSCODING_H
+#include <stddef.h>
+#ifdef _LIBICONV_H
+	#include<iconv.h>
+	#ifdef _MSC_VER
+	#pragma comment(lib,"iconv.lib")
+	#endif
+#endif
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+	int str_to_hex(char *string, char *cbuf);
+	void hex_to_str(unsigned char *dest, char *pbSrc);
+#ifdef _LIBICONV_H
+	int conv_charset(const char* dest,const char* pbSrc,const char *input,size_t ilen,char* output,size_t olen);
+#endif
+#ifdef __cplusplus
+}
+#endif
+#endif
