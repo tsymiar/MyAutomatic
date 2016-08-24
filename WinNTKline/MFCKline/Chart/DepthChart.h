1f62f53e86751330fdec5ae47b399f5af98aaacf
tsymiar <909006258@qq.com>
2016-08-24 12:52:58 +0800

    class changed to Singleton

--- WinNTKline/MFCKline/Chart/DepthChart.h
+++ WinNTKline/MFCKline/Chart/DepthChart.h
@@ -5,18 +5,13 @@
 class DepthChart
 {
 public:
-	DepthChart();
+	static DepthChart* getDepth();
 	virtual ~DepthChart();
-
-public:
-	OGLKview::Item item;
-
-public:
 	void DrawItem(OGLKview::Item item, int col);
 	int DrawItem(OGLKview::Item item,bool mode=false);
 	void FillChart(bool unfurl);
 	void DelItem(int item);
-
+	OGLKview::Item item;
 private:
 	float Py, Pm;
 	int hour = 0;
@@ -24,11 +19,11 @@ private:
 	int MAX_COL = 17;
 	OGLKview Okv;
 	OGLKview::Color4f color = { 1,1,1,1 };
-
+	static DepthChart *depth;
 private:
+	DepthChart();
 	void fillitem();
 	void drawList(void);
 	void fillAskbid(OGLKview::Point pt);
 };
-
 #endif // !CHART_DEPTH_H_
