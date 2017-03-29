b51eea4d6fdeebd857c079487d12e40f0baa53f9
tsymiar <909006258@qq.com>
2017-03-29 09:36:15 +0800

    this is a back-up version which may can not complie succeed

---
+++ WinNTKline/MFCKline/font/FontNehe.h
@@ -0,0 +1,92 @@
+#ifndef FONT_NEHE_H_
+#define FONT_NEHE_H_
+
+// FreeType Headers
+#include <font/ft2build.h>
+#include <font/freetype/freetype.h>
+#include <font/freetype/ftglyph.h>
+#include <font/freetype/ftoutln.h>
+#include <font/freetype/fttrigon.h>
+
+// OpenGL Headers
+#include <windows.h>                                      // (The GL Headers Need It)
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+// Some STL Headers
+#include <vector>
+#include <string>
+
+// Using The STL Exception Library Increases The
+// Chances That Someone Else Using Our Code Will Correctly
+// Catch Any Exceptions That We Throw.
+#include <stdexcept>
+#pragma comment(lib, "freetype.lib") 
+// MSVC Will Spit Out All Sorts Of Useless Warnings If
+// You Create Vectors Of Strings, This Pragma Gets Rid Of Them.
+#pragma warning(disable: 4244)
+#pragma warning(disable: 4786)
+#pragma warning(disable: 4793)
+// Wrap Everything In A Namespace, That Way We Can Use A Common
+// Function Name Like "print" Without Worrying About
+// Overlapping With Anyone Else's Code.
+namespace freetype {
+
+	// Inside Of This Namespace, Give Ourselves The Ability
+	// To Write Just "vector" Instead Of "std::vector"
+	using std::vector;
+
+	// Ditto For String.
+	using std::string;
+
+	// This Holds All Of The Information Related To Any
+	// FreeType Font That We Want To Create. 
+	struct font_data {
+		float h;                                        // Holds The Height Of The Font.
+		GLuint * textures;                                  // Holds The Texture Id's
+		GLuint list_base;                                   // Holds The First Display List Id
+
+															// The Init Function Will Create A Font With
+															// The Height h From The File fname.
+		void init(const char * fname, unsigned int h);
+
+		// Free All The Resources Associated With The Font.
+		void clean();
+	};
+	// The Flagship Function Of The Library - This Thing Will Print
+	// Out Text At Window Coordinates X, Y, Using The Font ft_font.
+	// The Current Modelview Matrix Will Also Be Applied To The Text.
+	__declspec(dllexport) void textout(const font_data &ft_font, float x, float y, const char *fmt, ...);
+	int DrawGLScene(GLvoid);
+}                                               // Close The Namespace
+typedef struct FT_LibraryRec_
+{
+	FT_Memory memory; /* library's memory manager */
+
+	FT_Int version_major;
+	FT_Int version_minor;
+	FT_Int version_patch;
+
+	FT_UInt num_modules;
+	FT_Module modules[FT_MAX_MODULES]; /* module objects */
+
+	FT_ListRec renderers; /* list of renderers */
+	FT_Renderer cur_renderer; /* current outline renderer */
+	FT_Module auto_hinter;
+
+	FT_Byte* raster_pool; /* scan-line conversion */
+						  /* render pool */
+	FT_ULong raster_pool_size; /* size of render pool in bytes */
+
+	//FT_DebugHook_Func debug_hooks[4];
+
+#ifdef FT_CONFIG_OPTION_SUBPIXEL_RENDERING
+	//FT_LcdFilter lcd_filter;
+	FT_Int lcd_extra; /* number of extra pixels */
+	FT_Byte lcd_weights[7]; /* filter weights, if any */
+	//FT_Bitmap_LcdFilterFunc lcd_filter_func; /* filtering callback */
+#endif
+
+} FT_LibraryRec;
+
+#endif // !FONT_NEHE_H_
--- WinNTKline/QtKline/QtKline/MyOglWdg.cpp
+++ WinNTKline/QtKline/QtKline/MyOglWdg.cpp
@@ -1,12 +1,147 @@
 #include "MyOglWdg.h"
 
-QMyOglWdg::QMyOglWdg(QWidget *parent)
-	: QOpenGLWidget(parent)
+QMyOglWdg::QMyOglWdg(QWidget* parent, const char* name, bool fs)
+	: QGLWidget(parent)
 {
-	
+	fullscreen = fs;
+	setGeometry(0, 0, 640, 480);
+	xRot = yRot = zRot = 0.0;
+	zoom = -5.0;
+	xSpeed = ySpeed = 0.0;
+
+	filter = 0;
+
+	light = false;
+
+	setWindowTitle("tsymiar's Tutorial");
+
+	if (fullscreen)
+		showFullScreen();
 }
 
 QMyOglWdg::~QMyOglWdg()
 {
 
+}
+
+void QMyOglWdg::initializeGL()
+{
+	kv.InitGraph();
+}
+
+void QMyOglWdg::paintGL()
+{
+
+	kv.GetMarkDatatoDraw();
+	glBindTexture(GL_TEXTURE_2D, texture[filter]);
+	xRot += xSpeed;
+	yRot += ySpeed;
+}
+
+void QMyOglWdg::resizeGL(int width, int height)
+{
+	if (height == 0)
+	{
+		height = 1;
+	}
+	glViewport(0, 0, (GLint)width, (GLint)height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, (GLfloat)width / (GLfloat)height, 0.1, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void QMyOglWdg::keyPressEvent(QKeyEvent * e)
+{
+	switch (e->key())
+	{
+	case Qt::Key_F2:
+		fullscreen = !fullscreen;
+		if (fullscreen)
+		{
+			showFullScreen();
+		}
+		else
+		{
+			showNormal();
+			setGeometry(0, 0, 640, 480);
+		}
+		updateGL();
+		break;
+	case Qt::Key_L:
+		light = !light;
+		if (!light)
+		{
+			glDisable(GL_LIGHTING);
+		}
+		else
+		{
+			glEnable(GL_LIGHTING);
+		}
+		updateGL();
+		break;
+	case Qt::Key_F:
+		filter += 1;;
+		if (filter > 2)
+		{
+			filter = 0;
+		}
+		updateGL();
+		break;
+	case Qt::Key_Period:
+		zoom -= 0.2;
+		updateGL();
+		break;
+	case Qt::Key_PageUp:
+		zoom += 0.2;
+		updateGL();
+		break;
+	case Qt::Key_Up:
+		xSpeed -= 0.01;
+		updateGL();
+		break;
+	case Qt::Key_Down:
+		xSpeed += 0.01;
+		updateGL();
+		break;
+	case Qt::Key_Right:
+		ySpeed += 0.01;
+		updateGL();
+		break;
+	case Qt::Key_Left:
+		ySpeed -= 0.01;
+		updateGL();
+		break;
+	case Qt::Key_Escape:
+		close();
+	}
+}
+
+void QMyOglWdg::loadGLTextures()
+{
+	QImage tex, buf;
+	if (!buf.load("./data/test.bmp"))
+	{
+		qWarning("Could not read image file, using single-color instead.");
+		QImage dummy(128, 128, QImage::Format_RGB32);
+		dummy.fill(QColor("green").rgb());
+		buf = dummy;
+	}
+	tex = QGLWidget::convertToGLFormat(buf);
+
+	glGenTextures(3, &texture[0]);
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, tex.width(), tex.height(), 0,
+		GL_RGBA, GL_UNSIGNED_BYTE, tex.bits());
+	glBindTexture(GL_TEXTURE_2D, texture[1]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, tex.width(), tex.height(), 0,
+		GL_RGBA, GL_UNSIGNED_BYTE, tex.bits());
+	glBindTexture(GL_TEXTURE_2D, texture[2]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
 }
