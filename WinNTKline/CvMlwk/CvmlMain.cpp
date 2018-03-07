2f6e888aa8781c88547d7ed1f111b3980b83de40
tsymiar <909006258@qq.com>
2018-03-08 00:41:26 +0800

    [CvMlwk] convert CvMl to CvMlwk dir/file/proj

---
+++ WinNTKline/CvMlwk/CvmlMain.cpp
@@ -0,0 +1,28 @@
+//CVML
+#ifdef WIN32
+#include "../MfcUtil/opencv/CvimgMat.h"
+#include "../MfcUtil/tensor/ShannonEntropy.h"
+#else
+#ifdef _DEBUG
+#error Only Release can work normally with x64/Python!
+#endif
+#include "../MfcUtil/tensor/CPyTensor.hpp"
+#endif
+int main()
+{
+#ifdef WIN32
+	const cv::String img = "../MfcUtil/image/taoxi.png";
+	CvimgMat vm;
+#undef CVML
+#ifdef CVML
+	vm.cvmat_test();
+#endif
+	cv::Mat mat = vm.getImageMat(img, 0);
+	std::cout << "ShannonEntropy -> [" << img << "] = " << ShannonEntropy::getInstance()->SingleEntropy(mat) << std::endl;
+	while (1);
+#else
+	CPyTensor tf;
+	tf.testPyfunc();
+#endif
+    return 0;
+}
