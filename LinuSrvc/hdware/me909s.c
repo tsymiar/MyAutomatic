782f46161ad08fc4185440818a8cad270ef7e070
tsymiar <909006258@qq.com>
2019-07-08 22:27:15 +0800

    [hdware] fix me909s.c issue.

--- LinuSrvc/hdware/me909s.c
+++ LinuSrvc/hdware/me909s.c
@@ -14,15 +14,32 @@
 #include <errno.h>
 #include <termios.h>
 
-#define MAX_BUFF	1024
-#define MAX_GET 	24
+#define MAX_BUF_LEN	1024
 
-void serial_init(int fd)
+int main(int argc, int **argv)
 {
+    fd_set rdfds;
+    FD_ZERO(&rdfds);
+    struct timeval tv;
+    tv.tv_sec = 1;
+    tv.tv_usec = 500;
+    int me_fd, ret, rd_stdin;
+    int flag = 0;
+    int times = 0;
+    char buff[MAX_BUF_LEN], rply[MAX_BUF_LEN];
     struct termios options;
+
+    me_fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY);
+    if (me_fd == -1) {
+        perror("Can't open /dev/ttyUSB0 - \n");
+        return 0;
+    } else {
+        fcntl(me_fd, F_SETFL, 0);
+    }
+
     bzero(&options, sizeof(options));
 
-    tcgetattr(fd, &options);
+    tcgetattr(me_fd, &options);
     //位掩码方式激活本地连接、使能接收
     options.c_cflag |= (CLOCAL | CREAD);
 
@@ -33,82 +50,60 @@ void serial_init(int fd)
     options.c_cflag |= CS8;
     options.c_cflag &= ~CSTOPB;
     options.c_iflag |= IGNPAR;
-    tcflush(fd, TCIFLUSH);
+    tcflush(me_fd, TCIFLUSH);
 
     options.c_oflag = 0;
     options.c_lflag = 0;
 
-    tcflush(fd, TCIOFLUSH);
-    if (0 == tcsetattr(fd, TCSANOW, &options)) {
+    tcflush(me_fd, TCIOFLUSH);
+    if (0 == tcsetattr(me_fd, TCSANOW, &options)) {
         printf("Serial tty is ready...\n");
     }
-}
 
-
-
-int main(int argc, int **argv)
-{
-    fd_set rdfds;
-    struct timeval tv;
-    int me_fd, ret, rd_stdin;
-    int flag = 0;
-    char buff[MAX_BUFF], replay[MAX_BUFF];
-
-    me_fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY);
-    if (me_fd == -1) {
-        perror("Can't open /dev/ttyUSB0 - \n");
-        return 0;
-    } else {
-        fcntl(me_fd, F_SETFL, 0);
-    }
-
-    serial_init(me_fd);
+    memset(buff, 0, sizeof(buff));
+    strcpy(buff, "AT+CPIN?\r\n");
 
     while (!flag)
     {
-        FD_ZERO(&rdfds);
-        FD_SET(me_fd, &rdfds);
-        tv.tv_sec = 1;
-        tv.tv_usec = 500;
-
-        memset(buff, 0, sizeof(buff));
-        strcpy(buff, "ATI\r\n");
         write(me_fd, buff, sizeof(buff));
         ret = select(me_fd + 1, &rdfds, NULL, NULL, &tv);
-        if (ret < 0)
+        if (ret < 0) {
             perror("select");
-        else if (ret == 0)
-            printf("Select state TIMEOUT!\n");
-        else
-        {
+            break;
+        } else if (ret == 0) {
+            times++;
+            if (times > 1000) {
+                printf("Timeout too many times, exit.\n");
+                break;
+            }
+            printf("Select state(%d) TIMEOUT!\n", times);
+            FD_SET(me_fd, &rdfds);
+            continue;
+        } else {
             printf("State changed inside 1.5s sum ret = %d.\n", ret);
             if (FD_ISSET(0, &rdfds)) {
                 flag = 1;
-                memset(replay, 0, sizeof(replay));
-                rd_stdin = read(0, replay, sizeof(replay));
-                printf("Data from stdin = %d:\n--- %s.\n", rd_stdin, replay);
+                memset(rply, 0, sizeof(rply));
+                rd_stdin = read(0, rply, sizeof(rply));
+                printf("Data from usb = %d:\n--- %s.\n", rd_stdin, rply);
             }
         }
         if (flag) {
             flag = 0;
-            memset(buff, 0, sizeof(buff));
-            printf("Input serial command: ");
-            if (scanf("%64s", replay) == -1) {
-                printf("Bytes beyound limit(64)!\n");
-            }
-            strcpy(buff, replay);
-            strcat(buff, "\r\n");
-            write(me_fd, buff, sizeof(buff));
-
-            memset(replay, 0, sizeof(replay));
-            read(me_fd, replay, sizeof(replay));
-            printf("--- %s\n", replay);
-
         } else {
-            memset(replay, 0, sizeof(replay));
-            read(me_fd, replay, sizeof(replay));
-            printf("--- %s\n", replay);
+            memset(rply, 0, sizeof(rply));
+            read(me_fd, rply, sizeof(rply));
+            printf("--------\n%s\n--------\n", rply);
         }
+        memset(buff, 0, sizeof(buff));
+        printf("New serial command: ");
+        if (scanf("%64s", rply) == -1) {
+            printf("Bytes beyound limit(1~64)!\n");
+            break;
+        }
+        strcpy(buff, rply);
+        strcat(buff, "\r\n");
     }
     close(me_fd);
+    return times;
 }
