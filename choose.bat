0d1b8f9823cb6061d196bec369ede9d1a10c7c10
tsymiar <909006258@qq.com>
2020-07-12 02:18:02 +0800

    batch script to set environment variables

---
+++ choose.bat
@@ -0,0 +1,26 @@
+<# : chooser.bat
+:: launches a File... Open sort of file chooser and outputs choice(s) to the console
+
+@echo off
+setlocal enabledelayedexpansion
+
+set local=BOOST CEFDIR QTDIR OPENCV OPENSSL PTHD_LIB86 JAVA_HOME
+set t=%local%
+:loop
+for /f "tokens=1*" %%a in ("%t%") do (
+    echo Choose "%%a" path by select dialog
+    for /f "delims=" %%I in ('powershell -noprofile "iex (${%~f0} | out-string)"') do (
+        echo "%%a = %%~I"
+	    setx %%a %%~I
+    )
+    set t=%%b
+)
+if defined t goto :loop
+goto :EOF
+
+: end Batch portion / begin PowerShell hybrid chimera #>
+
+Add-Type -AssemblyName System.Windows.Forms
+$f = new-object Windows.Forms.FolderBrowserDialog
+[void]$f.ShowDialog()
+$f.SelectedPath
