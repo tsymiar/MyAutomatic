c1daaabd742ec886c952035cb77f23e463bf9501
tsymiar <909006258@qq.com>
2025-12-08 21:49:16 +0800

    [media] fix build files

--- LinxSrvc/pyext/make.sh
+++ LinxSrvc/pyext/make.sh
@@ -4,6 +4,7 @@ if [ "${1}" == "clean" ]; then
     rm -rvf dist build
     rm -rvf ./*.egg-info
 else
+    python -m pip install --upgrade pip
     python -m pip install --upgrade --break-system-packages pip
     pip install --break-system-packages build
     python -m build
